640e88d74db1c13c60f06f83aacac6376fb26568
enkhboldzu <enkhboldzu@gmail.com>
2026-07-27 12:53:55 +0800

    4

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex min-h-screen items-center justify-center bg-gray-50">
-    <h1 class="text-3xl font-semibold text-gray-800">Ariunmur</h1>
+    <h1 class="text-3xl font-semibold text-gray-800">Ariunmur & Enkhbold</h1>
   </div>
 </template>
